40a3e12b87255de6a2dde99179c2784e1e754f96
Bea-Araujo <beatriz.a.0702@gmail.com>
2022-10-28 13:49:13 -0300

    base do projeto react criada

---
+++ src/routes.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { BrowserRouter, Routes as Switch, Route } from 'react-router-dom'
+import { Login } from './pages/Login/Login'
+
+export const Routes = () => {
+    return (
+        <BrowserRouter>
+            <Switch>
+                <Route to='/login' element={<Login />} />
+            </Switch>
+        </BrowserRouter>
+    )
+}
